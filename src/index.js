c151e28c3dc78d1eedd4e3794bd1580146af4fb9
Emil Bay <hello@tixz.dk>
2013-12-05 16:56:01 +0100

    Testing Codeship build system

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-  var body = 'Hello World';
+  var body = 'Hello World 2';
   res.setHeader('Content-Type', 'text/plain');
   res.setHeader('Content-Length', body.length);
   res.end(body);

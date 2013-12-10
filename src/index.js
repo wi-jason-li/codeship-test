7aab202642fcd9967edebf21afddce00f6cb4e3b
OpenShift guest <howard.chow@wellintech.com.hk>
2013-12-09 22:27:04 -0500

    Continue Now

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-  var body = 'Hello World 234 Continue';
+  var body = 'Hello World 234 Continue Now';
   res.setHeader('Content-Type', 'text/plain');
   res.setHeader('Content-Length', body.length);
   res.end(body);
